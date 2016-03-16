319a0068d944aa7ebc0525dd58fcbc0ac1a95dc2
Larry Ogrodnek <ogrodnek@gmail.com>
2016-03-16 10:21:27 -0700

    add cache headers

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,7 @@ R=`dirname $0`
 export AWS_DEFAULT_PROFILE=ogrodnek
 
 rm -rf $R/.build
-cactus build && aws s3 sync $R/.build s3://photos.ogrodnek.com
+cactus build
+
+aws s3 sync $R/.build s3://photos.ogrodnek.com --exclude "*.jpg" --cache-control max-age=300
+aws s3 sync $R/.build s3://photos.ogrodnek.com --include "*.jpg" --cache-control max-age=1800
